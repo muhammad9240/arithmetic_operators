eebee34d4140268a66eace27cf712e85918d20f5
muhammad9240 <muhammad92401111@gmail.com>
2023-04-26 11:52:30 +0500

    dfbdf

--- bin/arithmetic_operator01.dart
+++ bin/arithmetic_operator01.dart
@@ -3,9 +3,10 @@
 Create a variable 'num_one'  and assign it the value of the int 5.
 Create a variable 'num_two'   and assign it the value of the int 4.
 Find the sum of  'num_one' and 'num_two' then assign to 'variable_total'
-
+ //write your code here
 */
 void main() {
-  //write your code here
-  
+  int num_one = 5, num_two = 4;
+  int jami = num_one + num_two;
+  print(jami);
 }
